a4a79e78a2becbf0091debbf618c42798564edae
Knyazeva Anna <you@example.com>
2022-12-11 16:13:49 +0300

    actors

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,11 @@
 class Actor
   include Mongoid::Document
   include Mongoid::Timestamps
+
+  field :surname, type: String
+  field :name, type: String
+  field :year, type: Integer
+  field :country, type: String
+
+  validates :surname, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :serials
+  resources :actors
 
   root "serials#index"
 end
